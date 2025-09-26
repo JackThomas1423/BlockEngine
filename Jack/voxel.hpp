0bf25b62ae9e88143279df3f5f6dbceb50c8f835
JackThomas1423 <144173595+JackThomas1423@users.noreply.github.com>
2025-09-26 13:01:19 -0700

    Add: comments

--- Jack/voxel.hpp
+++ Jack/voxel.hpp
@@ -4,8 +4,9 @@
 #define CHUNK_HEIGHT 16
 #define CHUNK_DEPTH 16
 
-//uint8_t is the type used for the colors. this maybe turned into a struct if we need more voxel spesific data is needed
-//add chunk spesific data as needed
+// uint8_t is the type used for the colors. this maybe turned into a struct if we need more voxel specific data is needed
+// add chunk specific data as needed
 struct Chunk {
     uint8_t data[CHUNK_WIDTH][CHUNK_HEIGHT][CHUNK_DEPTH];
 };
+
