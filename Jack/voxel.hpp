82686b611a5b36bcd5189ca9d883b8d8a770339e
JackThomas1423 <144173595+JackThomas1423@users.noreply.github.com>
2025-09-26 13:53:07 -0700

    Add: outline for basic voxel coords

--- Jack/voxel.hpp
+++ Jack/voxel.hpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <vector>
 
 #define CHUNK_WIDTH 16
 #define CHUNK_HEIGHT 16
@@ -6,7 +7,11 @@
 
 // uint8_t is the type used for the colors. this maybe turned into a struct if we need more voxel specific data is needed
 // add chunk specific data as needed
+
+typedef uint8_t ColorId;
+
 struct Chunk {
-    uint8_t data[CHUNK_WIDTH][CHUNK_HEIGHT][CHUNK_DEPTH];
+    ColorId data[CHUNK_WIDTH][CHUNK_HEIGHT][CHUNK_DEPTH];
 };
 
+std::vector<float> voxelToMesh();
